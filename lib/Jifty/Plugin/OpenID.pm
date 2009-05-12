4e219c9294f882e31541f64a6eff1f4c06f05105
Cornelius Lin <c9s@aiink.com>
2009-05-12 04:41:03 +0000

    - provide UserAgent class option for OpenID plugin.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/plugins/Jifty-Plugin-OpenID@6987 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/OpenID.pm
+++ lib/Jifty/Plugin/OpenID.pm
@@ -3,9 +3,8 @@ use warnings;
 
 package Jifty::Plugin::OpenID;
 use base qw/Jifty::Plugin/;
-use LWPx::ParanoidAgent;
 
-our $VERSION = '0.9';
+our $VERSION = '0.10';
 
 =head1 NAME
 
@@ -17,11 +16,39 @@ Provides OpenID authentication for your app
 
 =cut
 
+sub init {
+    my $self = shift;
+    my %opt = @_;
+    my $ua_class = $self->get_ua_class;
+    eval "require $ua_class";
+}
+
+sub get_ua_class {
+    return Jifty->config->app('OpenIDUA') 
+                || $ENV{OpenIDUserAgent} 
+                || 'LWPx::ParanoidAgent' ;
+}
+
+sub new_ua {
+    my $class = shift;
+    my $ua;
+    my $ua_class = $class->get_ua_class;
+    if( $ua_class eq 'LWPx::ParanoidAgent' ) {
+         $ua = LWPx::ParanoidAgent->new(
+                        whitelisted_hosts => [ $ENV{JIFTY_OPENID_WHITELIST_HOST} ]
+                     );
+    }
+    else {
+        $ua = $ua_class->new;
+    }
+    return $ua;
+}
+
+
 sub get_csr {
     my $class = shift;
-
     return Net::OpenID::Consumer->new(
-        ua              => LWPx::ParanoidAgent->new( whitelisted_hosts => [ $ENV{JIFTY_OPENID_WHITELIST_HOST} ] ),
+        ua              => $class->new_ua ,
         cache           => Cache::FileCache->new,
         args            => scalar Jifty->handler->cgi->Vars,
         consumer_secret => Jifty->config->app('OpenIDSecret'),
